23d0fb477706476cbe33bb78acbcd9d55111a09a
Nataniel Martin <nataniel.martin@shopify.com>
2020-12-14 12:33:54 +0100

    Remove EXCLUDE ARCHS build settings

--- FiskalySDK.xcframework/ios-arm64_armv7/FiskalySDK.framework/Headers/FiskalyClient.h
+++
@@ -1,4 +0,0 @@
-
-extern const char* _fiskaly_client_invoke(const char* request);
-
-extern void _fiskaly_client_free(const char* response);
--- FiskalySDK.xcframework/ios-arm64_armv7/FiskalySDK.framework/Headers/FiskalySDK.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for FiskalySDK.
-FOUNDATION_EXPORT double FiskalySDKVersionNumber;
-
-//! Project version string for FiskalySDK.
-FOUNDATION_EXPORT const unsigned char FiskalySDKVersionString[];
