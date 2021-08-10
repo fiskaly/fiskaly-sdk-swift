7d4f632ff692c05562422c52123afe5a1437d04d
Angela Brett <angela.brett@fiskaly.com>
2021-08-10 18:34:25 +0200

    Changed iOS version requirement of client Swift Package to match that of the SDK

--- FiskalyClient/Package.swift
+++ FiskalyClient/Package.swift
@@ -7,7 +7,7 @@ let client = "com.fiskaly.client-ios-all-v2.0.0"
 let package = Package(
     name: client,
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
